3961cf72dc060f4080e909974cff00679c36db49
Gerry <gerrywiegand@gmail.com>
2025-11-11 14:57:55 -0500

    finished

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
-import requests
-import json
+import json  # noqa: F401
+
+import requests  # noqa: F401
+
 
 class GetRequester:
+    url = (
+        "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    )
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        raw = self.get_response_body()
+        return json.loads(raw)
